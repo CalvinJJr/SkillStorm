86e532dd160b5f55768c2d8a7fafd7a2829ea737
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-22 13:49:01 -0400

    Add files via upload

---
+++ Loop Challenge.py
@@ -0,0 +1,3 @@
+number = 10
+for number in range(11):
+    print(number)
---
+++ Student Grading App.py
@@ -0,0 +1,15 @@
+# Grading is based on 100 point basis
+
+x = 85
+print(x)
+
+if x >= 90:
+    print("You got an A!")
+elif x <= 89:
+    print("You got a B!")
+elif x <= 79:
+    print("You got a C!")
+elif x <= 69:
+    print("You got a D!")
+else:
+    print("You got an F!")
