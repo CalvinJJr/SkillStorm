a00922234271f39bc804f56552ccdaa9529eed0a
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-22 18:05:47 -0400

    Add files via upload

---
+++ Module Challenge.py
@@ -0,0 +1,12 @@
+# creating a module that handles basic arithmetic of two numbers
+
+import Calculator as c
+
+print(c.multiply(80, 2))
+
+print(c.add(4, 9))
+
+print(c.divide(7,4))
+
+print(c.subtract(23, 4))
+
