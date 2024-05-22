4200cf4ab6be9ddf0e820787f9bf3573b8b015b7
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-22 17:34:26 -0400

    Add files via upload

---
+++ Calculator Challenge.py
@@ -0,0 +1,27 @@
+# Calculator for adding, subtracting, multiplying, and dividing integers.
+
+def add(number1, number2):
+    s_number = number1 + number2
+    return s_number
+
+
+def subtract(number1, number2):
+    r_number = number1 - number2
+    return r_number
+
+
+def multiply(number1, number2):
+    m_number = number1 * number2
+    return m_number
+
+
+def divide(number1, number2):
+    q_number = number1 / number2
+    return q_number
+
+
+
+print(add(8,15))
+
+
+
