6e3cb3f116036b589562bd367189d3d96efddda3
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-22 21:19:47 -0400

    Add files via upload

--- Inheritance Challenge.py
+++ Inheritance Challenge.py
@@ -1,10 +1,13 @@
 # inheritance challenge
 class Person:
     # Instance variables
-    has_title = True
+    has_identity = True
+
+    if has_identity:
+        print("Hi, I'm a person.")
 
     def speaks(self):
-        print("Hi, I'm a person!")
+        print("I'm off to do whatever!\n")
 
 
 person = Person()
@@ -13,8 +16,10 @@ person.speaks()
 
 class Customer(Person):
 
+    has_identity = False
+
     def speaks(self):
-        print("I'm spending money!")
+        print("I'm a customer, I'm spending money!\n")
 
 
 customer = Customer()
@@ -23,11 +28,11 @@ customer.speaks()
 
 class Developer(Person):
 
+    has_identity = True
+
     def speaks(self):
-        print("I'm off to code!")
+        print("I'm a developer, I'm off to code!\n")
 
 
 developer = Developer()
 developer.speaks()
-
-
